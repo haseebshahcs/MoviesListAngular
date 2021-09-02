007d5d316d58d8a7e9c455e8d7e247357990f75a
Haseeb Shah <haseeb.shah@telenorpk01.onmicrosoft.com>
2021-09-02 10:57:44 +0500

    movies componenet added

--- src/app/movies/movies-list/movies-list.component.ts
+++ src/app/movies/movies-list/movies-list.component.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MoviesListComponent implements OnInit {
 
+  movies: any;
   constructor() { }
 
   ngOnInit(): void {
-  }
+    setTimeout(() => {
+      this.movies = [
+        { title: 'Spiderman', releaseDate: new Date(), price: 50 },
+        { title: 'Spiderwomen', releaseDate: new Date(-1), price: 50 }
+      ]
+    }, 2000);
 
+  }
 }
