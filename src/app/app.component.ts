2c7c7e91ea0ae2b1721efdcb3de251caa92404a2
Haseeb Shah <haseeb.shah@telenorpk01.onmicrosoft.com>
2021-09-12 14:35:33 +0500

    new changes added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,20 +3,19 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
   ngOnInit(): void {
     this.moviesInTheaters = [
       { title: 'Spiderman', releaseDate: new Date(), price: 50 },
-      { title: 'Spiderwomen', releaseDate: new Date(-1), price: 50 }
+      { title: 'Spiderwomen', releaseDate: new Date(-1), price: 50 },
     ];
-    this.moviesFutureReleases = 
-    [
+    this.moviesFutureReleases = [
       { title: 'Spiderman1', releaseDate: new Date(), price: 50 },
-      { title: 'Spiderwomen2', releaseDate: new Date(-1), price: 50 }
+      { title: 'Spiderwomen2', releaseDate: new Date(-1), price: 50 },
     ];
   }
-  moviesInTheaters : any;
-  moviesFutureReleases : any;
+  moviesInTheaters: any;
+  moviesFutureReleases: any;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MoviesListComponent } from './movies/movies-list/movies-list.component';
+import { GenericListComponent } from './utilities/generic-list/generic-list.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    MoviesListComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, MoviesListComponent, GenericListComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
